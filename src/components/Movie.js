e59a85bdaa3988cc807c24516b9d17ff3fc405df
Naasir Salafee <naasirbush@icloud.com>
2024-11-10 10:27:59 -0800

    added hearts

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,7 +8,7 @@ function Movie({ movie, toggleFavorite }) {
     <div className="movie">
       <h3>{title} ({year})</h3>
       <button onClick={() => toggleFavorite(id)}>
-        {isFavorite ? 'Unfavorite' : 'Favorite'}
+        {isFavorite ? '💙Unfavorite' : '♡Favorite'}
       </button>
       <Link to={`/movies/${id}`}>View Details</Link>
     </div>
