9176cea3c502277e10be74f1306785c8100d5ad3
Naasir Salafee <naasirbush@icloud.com>
2024-11-10 10:31:49 -0800

    added image to code of home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,8 @@ function HomePage() {
       <div>
         <h1>Welcome to Favorite Movies App</h1>
         <p>Discover and save your favorite movies!</p>
+        <img className='responsive-image' src="/istockphoto-2101122136-1024x1024.jpg" alt="A woman holding a remote" />
+
       </div>
     );
   }
